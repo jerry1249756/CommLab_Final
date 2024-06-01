2ce8500033ebb837cd1758e7f67580d274a24c43
Chuang-Yu Chang <jimmy111923@gmail.com>
2024-06-02 01:33:58 +0800

    I wanna sleep

--- Huffman.py
+++ Huffman.py
@@ -1,3 +1,7 @@
+import numpy as np
+import soundfile as sf
+import sounddevice as sd
+import time
 class TreeNode:
     def __init__(self, symbol, freq):
         self.symbol = symbol
@@ -6,15 +10,24 @@ class TreeNode:
         self.right = None
 
 class HuffmanTree:
-    def __init__(self, sequence):
-        self.sequence = sequence
+    def __init__(self, _sequence, _nlevels):
+        self.data = _sequence
+        self.n_levels = _nlevels
+        self.quantize()
         self.freq_map = self.build_freq_map()
         self.root = self.build_huffman_tree()
         self.codes = self.generate_huffman_codes()
 
+    def quantize(self):
+        self.data_min = np.min(self.data)
+        self.data_max = np.max(self.data)
+        step_size = (self.data_max - self.data_min) / (self.n_levels-1)
+        # quantize
+        self.quantized_data = np.round((self.data - self.data_min) / step_size ).astype(int)
+
     def build_freq_map(self):
         freq_map = {}
-        for symbol in self.sequence:
+        for symbol in self.quantized_data:
             freq_map[symbol] = freq_map.get(symbol, 0) + 1
         return freq_map
 
@@ -41,9 +54,9 @@ class HuffmanTree:
         traverse(self.root)
         return codes
 
-    def encode(self, original_sequence):
+    def encode(self):
         encoded_sequence = []
-        for symbol in original_sequence:
+        for symbol in self.quantized_data:
             encoded_sequence += self.codes[symbol]
         return encoded_sequence
 
@@ -60,11 +73,37 @@ class HuffmanTree:
                 current_node = self.root
         return decoded_sequence
     
+    def test(self, fs = None):
+        enc_start = time.time()
+        encoded = self.encode()
+        enc_end = time.time()
+        dec_start = time.time()
+        decoded = self.decode(encoded)
+        dec_end = time.time()
+        self.prob_dist = np.bincount(self.quantized_data, minlength=self.n_levels)
+        temp = self.prob_dist[self.prob_dist > 0] / len(self.data)
+        print("============= Summary =============")
+        print(f"Number of Input Symbols: {len(self.data)}")
+        print(f"Number of Input Bits: {len(self.data) * np.log2(self.n_levels)}")
+        print(f"Number of Encoded Bits: {len(encoded)}")
+        print(f"Entropy: {-np.sum(temp * np.log2(temp))}")
+        print(f"Encoding Time: {enc_end - enc_start}")
+        print(f"Decoding Time: {dec_end - dec_start}")
+        step_size = (self.data_max - self.data_min) / (self.n_levels-1)
+        decoded_data = np.array(decoded)*step_size + self.data_min
+        sd.play(decoded_data, fs)
+        sd.wait()
+
+
+def epsilon_sequence(epsilon, length, n_symbols):
+    prob = [epsilon] + [(1 - epsilon) / (n_symbols - 1)] * (n_symbols - 1)
+    symbols = list(range(n_symbols))
+    sequence = np.random.choice(symbols, size=length, p=prob)
+    
+    return sequence
 if __name__ == '__main__':
-    sequence = [0, 1, 5, 9, 3, 5, 8]
-    huffman_tree = HuffmanTree(sequence)
-    encoded_sequence = huffman_tree.encode(sequence)
-    decoded_sequence = huffman_tree.decode(encoded_sequence)
-    print(encoded_sequence)
-    print(decoded_sequence)
+    #data = epsilon_sequence(0.99, 100000, 20)
+    data, fs = sf.read('handel.wav')
+    huffman_tree = HuffmanTree(data, 64)
+    huffman_tree.test(fs)
     
--- LZW.py
+++ LZW.py
@@ -1,23 +1,35 @@
+import numpy as np
+import time
+import soundfile as sf
+import sounddevice as sd
 class LZW:
-    def __init__(self, sequence):
-        self.sequence = sequence
-        self.dictionary = {}
+    def __init__(self, _sequence, _nlevels):
+        self.data = _sequence
+        self.n_levels = _nlevels
+        self.quantize()
+        self.initialize_dictionary()
+
+    def quantize(self):
+        self.data_min = np.min(self.data)
+        self.data_max = np.max(self.data)
+        step_size = (self.data_max - self.data_min) / (self.n_levels-1)
+        # quantize
+        self.quantized_data = np.round((self.data - self.data_min) / step_size ).astype(int)
 
     def initialize_dictionary(self):
         self.dictionary = {}
         cnt_dict = 0
-        for symbol in self.sequence:
+        for symbol in self.quantized_data:
             if tuple([symbol]) not in self.dictionary:
                 self.dictionary[tuple([symbol])] = cnt_dict
                 cnt_dict += 1
 
-    def encode(self, original_sequence):
-        self.initialize_dictionary()
+    def encode(self):
         cnt_dict = len(self.dictionary)
-        S = [original_sequence[0]]
-        C = [original_sequence[1]]
+        S = [self.quantized_data[0]]
         encoded_sequence = []
-        for cnt_seq in range(2, len(original_sequence)):
+        for cnt_seq in range(1, len(self.quantized_data)):
+            C = [self.quantized_data[cnt_seq]]
             W = S + C
             if tuple(W) in self.dictionary:
                 S = W + []
@@ -26,30 +38,66 @@ class LZW:
                 self.dictionary[tuple(W)] = cnt_dict
                 cnt_dict += 1
                 S = C + []
-            C = [original_sequence[cnt_seq]]
         W = S + C
-        if tuple(W) in self.dictionary:
-            encoded_sequence += [self.dictionary[tuple(W)]]
-        else:
-            encoded_sequence += [self.dictionary[tuple(S)]]
-            encoded_sequence += [self.dictionary[tuple(C)]]
-        
+        encoded_sequence += [self.dictionary[tuple(S)]]
+        self.complete_dict_len = len(self.dictionary)
         return encoded_sequence
     
     def decode(self, encoded_sequence):
-        self.initialize_dictionary()
         cnt_dict = len(self.dictionary)
         temp_dict = {y: x for x, y in self.dictionary.items()}
-        S = []
-        W = []
-        decoded_sequence = []
-        for symbol in encoded_sequence:
-            W = list(temp_dict[symbol])
-            decoded_sequence += W
-            if len(S) > 0:
-                temp_dict[cnt_dict] = S + [W[0]]
-                cnt_dict += 1
-            S = W + []
-
+        OLD = encoded_sequence[0]
+        NEW = -1
+        C = []
+        decoded_sequence = list(temp_dict[OLD])
+        for symbol in encoded_sequence[1:]:
+            NEW = symbol
+            #print(temp_dict)
+            if temp_dict.get(NEW, 0) == 0:
+                S = list(temp_dict[OLD])
+                S = S + C
+            else:
+                S = list(temp_dict[NEW])
+            decoded_sequence += S
+            C = [S[0]]
+            temp_dict[cnt_dict] = tuple(list(temp_dict[OLD]) + C)
+            cnt_dict += 1
+            OLD = NEW
         return decoded_sequence
-            
+    
+    def test(self, fs = None):
+        self.initialize_dictionary()
+        enc_start = time.time()
+        encoded = self.encode()
+        enc_end = time.time()
+        self.initialize_dictionary()
+        dec_start = time.time()
+        decoded = self.decode(encoded)
+        dec_end = time.time()
+        self.prob_dist = np.bincount(self.quantized_data, minlength=self.n_levels)
+        temp = self.prob_dist[self.prob_dist > 0] / len(self.data)
+        print("============= Summary =============")
+        print(f"Number of Input Symbols: {len(self.data)}")
+        print(f"Number of Input Bits: {len(self.data) * np.log2(self.n_levels)}")
+        print(f"Number of Encoded Bits: {len(encoded) * np.ceil(np.log2(self.complete_dict_len))}")
+        print(f"Entropy: {-np.sum(temp * np.log2(temp))}")
+        print(f"Encoding Time: {enc_end - enc_start}")
+        print(f"Decoding Time: {dec_end - dec_start}")
+        step_size = (self.data_max - self.data_min) / (self.n_levels-1)
+        decoded_data = np.array(decoded)*step_size + self.data_min
+        #sd.play(decoded_data, fs)
+        #sd.wait()
+
+def epsilon_sequence(epsilon, length, n_symbols):
+    symbols = list(range(n_symbols))
+    high_prob_symbol = np.random.choice(symbols)
+    prob = [(1 - epsilon) / (n_symbols - 1)] * n_symbols
+    prob[high_prob_symbol] = epsilon
+    sequence = np.random.choice(symbols, size=length, p=prob)
+    return sequence
+
+if __name__ == '__main__':
+    data = epsilon_sequence(0.99, 1000000, 20)
+    # data, fs = sf.read('handel.wav')
+    lzw = LZW(data, 20)
+    lzw.test()
